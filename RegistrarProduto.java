8b132c0b93b9e3ef425b3bae6e9ea1b4e15c20d7
MateusJuan <169060996+MateusJuan@users.noreply.github.com>
2024-10-30 19:54:52 -0300

    30/10/2024

--- RegistrarProduto.java
+++ RegistrarProduto.java
@@ -1,13 +1,22 @@
+
+import java.util.Locale;
 import java.util.Scanner;
 
-public class RegistrarProduto {
+
+public class RegistrarProduto{
     public static void main(String[] args){
+        Locale.setDefault(new Locale("en","US"));
         Scanner input = new Scanner(System.in);
 
         float preco;
+        String nome;
 
         System.out.println("Digite o preço do produto:");
-            preco = input.nextFloat();
-        System.out.printf("O preço do produto é %.2f",preco);
+        preco = input.next();
+
+        System.out.println("Digite o nome do procuto:");
+        nome = input.next();
+
+        System.out.printf("O preço do produto %s é .2f",nome,preco);
     }
 }
